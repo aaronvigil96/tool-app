3a0773217ff2218ef9f1ce5ad484e8ad6213fe1c
aaron <aaronvigil96@gmail.com>
2024-10-11 21:54:02 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+
+  devToolbar: {
+    enabled: false
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
